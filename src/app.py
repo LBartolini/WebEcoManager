47c27be8263a8af08526c89d950652d3d2caa12a
LBartolini <l.bartolini02@gmail.com>
2020-08-04 18:07:49 +0200

    Reserved page and login; Need to do auth and a lot of things...

--- src/app.py
+++ src/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, session
 from flask_mysqldb import MySQL
 import yaml, hashlib
 
 app = Flask(__name__, static_folder="static", static_url_path="/static/")
+app.secret_key = b'_5.y2l"F4Q5z%P\xEc]/'
 
 db = yaml.load(open('db.yaml'), Loader=yaml.BaseLoader)
 app.config['MYSQL_HOST'] = db['mysql_host']
@@ -18,11 +19,33 @@ def entry():
 
 @app.route('/home', methods=['GET'])
 def home():
-    return render_template('home.html')
+	if 'username' in session:
+		session.pop('username', None)
+	return render_template('home.html', saldo=-1)
 
-@app.route('/reserved', methods=['POST'])
+@app.route('/reserved', methods=['GET', 'POST'])
 def reserved():
-    return render_template('reserved.html')
+	logged_in = False
+	if request.method == 'GET':
+		if 'username' in session:
+			# already logged in
+			logged_in = True
+		else:
+			return redirect('/home')
+	else:
+		# login authentication
+		session['username'] = request.form['username']
+		logged_in = True
+    
+	if logged_in:
+		# TODO calcolare saldo dal database
+		return render_template('reserved.html', saldo=1460)
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+	# TODO insert data into db then redirect to reserved
+    return	redirect('/reserved')
 
 
 if __name__ == "__main__":
